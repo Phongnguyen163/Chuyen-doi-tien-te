297cbba65884ba9dfaecb0a6583152343547ee11
Phongnguyen163 <thanhphongnguyen99163@gmail.com>
2022-03-08 16:22:56 +0700

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+function showResult() {
+    G = document.getElementById('td').value;
+    T = document.getElementById('a').value;
+    U = document.getElementById('b').value;
+    if (T == "VietNam" && U == "USD") {
+        G=G/23000;
+    }
+    document.getElementById('4').innerText = "Result: " + G;
+}
